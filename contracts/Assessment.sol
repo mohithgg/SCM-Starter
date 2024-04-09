b08acfc3bdd03e942069b8280e922342bbc644a0
mohithgg <166399043+mohithgg@users.noreply.github.com>
2024-04-09 22:07:52 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,34 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    struct emp {
+        uint phoneno;
+        string name;
+        uint salary;
+    }
+    
+    emp[] public employees;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    string public dispmssg;
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    constructor(string memory initdispmssg) {
+        dispmssg = initdispmssg;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function getDispMssg() public view returns (string memory) {
+        return dispmssg;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function addEmployee(uint _phoneno, string memory _name, uint _salary) public payable{
+        require(_salary > 100000);
+        employees.push(emp(_phoneno, _name, _salary));
+        dispmssg="Employee added successfully";
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+    function retrieveEmployee(uint _index) public  {
+        require(_index < employees.length, "Employee does not exist");
+        dispmssg=employees[_index].name;
     }
+    
 }
